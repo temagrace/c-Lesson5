3032b0a4fe9dfc498c3db62ab5f8a85f22769472
temagrace <temagrace@hotmail.com>
2024-06-04 15:58:04 +0300

    Add files via upload

---
+++ lesson5/Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+int Fact(int n)
+{
+    if (n == 1 || n == 0)
+    {
+        return 1;
+    }
+    return n * Fact(n - 1);
+    Console.WriteLine(n);
+}
+Console.WriteLine("ВВЕДИТЕ ЧИСЛО");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write(Fact(n));
